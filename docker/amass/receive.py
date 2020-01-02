26b2d6b776f6ec28e2117c7e278d6e6aad55463d
th3-alch3m1st <an1sor0pous@protonmail.com>
2020-01-02 16:10:32 +0000

    code refactor

--- docker/amass/receive.py
+++ docker/amass/receive.py
@@ -6,39 +6,13 @@
 import pika
 import sys,os
 import time
+import app.spawn_subscriber
 from subprocess import Popen, PIPE
-
-try:
-    # Connect to RabbitMQ on the localhost, if different machine use IP/hostname
-    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', connection_attempts=5, retry_delay=5))
-
-    # https://www.programcreek.com/python/example/13392/pika.BlockingConnection
-    assert connection.is_open is True
-    assert connection.is_closed is False
-    sys.stdout.write('Connection to RabbitMQ OK')
-
-except Exception as e:
-    msg = ('ampq connection failed ({})'.format(str(e)))
-    sys.stdout.write(msg)
-
-channel = connection.channel()
-
-# 'test' exchange to send the message to
-channel.exchange_declare(exchange='test', exchange_type='direct')
-
-# The server will choose a random queue name
-result = channel.queue_declare(queue='', exclusive=True)
-queue_name = result.method.queue
 
 options = sys.argv[1:]
 if not options:
     sys.stderr.write("Usage: %s [passive] [brute] [perms]\n" % sys.argv[0])
     sys.exit(1)
-
-for move in options:
-    channel.queue_bind(exchange='test', queue=queue_name, routing_key=move)
-
-print(" [*] Waiting for logs. To exit press CTRL+C")
 
 def callback(ch, method, properties, body):
     opt = body.split(" ")
@@ -50,8 +24,4 @@ def callback(ch, method, properties, body):
         print stdout
         print('finished amass')
 
-# This specific callback should receive msg from hello queue
-channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-
-# Never ending loop to wait for messages
-channel.start_consuming()
+app.spawn_subscriber.rabbitmqConnection(options, callback)
--- docker/assetfinder/receive.py
+++ docker/assetfinder/receive.py
@@ -5,53 +5,36 @@
 '''
 import pika
 import sys,os
-import time
+import app.spawn_subscriber
+from datetime import datetime
 from subprocess import Popen, PIPE
-
-try:
-    # Connect to RabbitMQ on the localhost, if different machine use IP/hostname
-    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', connection_attempts=5, retry_delay=5))
-
-    # https://www.programcreek.com/python/example/13392/pika.BlockingConnection
-    assert connection.is_open is True
-    assert connection.is_closed is False
-    sys.stdout.write('Connection to RabbitMQ OK')
-
-except Exception as e:
-    msg = ('ampq connection failed ({})'.format(str(e)))
-    print(msg)
-
-channel = connection.channel()
-
-# 'test' exchange to send the message to
-channel.exchange_declare(exchange='test', exchange_type='direct')
-
-# The server will choose a random queue name
-result = channel.queue_declare(queue='', exclusive=True)
-queue_name = result.method.queue
 
 options = sys.argv[1:]
 if not options:
     sys.stderr.write("Usage: %s [passive] [brute] [perms]\n" % sys.argv[0])
     sys.exit(1)
 
-for move in options:
-    channel.queue_bind(exchange='test', queue=queue_name, routing_key=move)
-
-print(" [*] Waiting for logs. To exit press CTRL+C")
-
 def callback(ch, method, properties, body):
+    # body variable will contain `passive domain.com`
     opt = body.split(" ")
     print(" [x] Starting %r scans for %r" % (method.routing_key, opt[1]))
+
+    # Grab date and add to filename
+    now = datetime.now()
+    date = now.strftime("%Y%m%d%H%M%S")
+
+    # Check if folder exists
+    filepath = '/tools/output/' + opt[1]
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+
+    # Start scan
     if method.routing_key == 'passive':
         print("Start assetfinder")
-        process = Popen(['/go/bin/assetfinder', opt[1]], stdout=PIPE, stderr=PIPE)
-        stdout, stderr = process.communicate()
-        print stdout
+        with open(filepath + "/assetfinder-" + opt[1] + "." + date,"wb") as out, open(filepath + "/assetfinder-errors-" + opt[1] + "." + date,"wb") as err:
+            Popen(['/go/bin/assetfinder', opt[1]], stdout=out, stderr=err)
+        #stdout, stderr = process.communicate()
+        #print stdout
         print("finished assetfinder")
 
-# This specific callback should receive msg from hello queue
-channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-
-# Never ending loop to wait for messages
-channel.start_consuming()
+app.spawn_subscriber.rabbitmqConnection(options, callback)
